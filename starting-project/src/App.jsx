49a760d6ca01fafe031eea70280d9360a806ba3f
Soeun0 <144868086+Soeun0@users.noreply.github.com>
2023-10-04 20:56:36 +0900

    3주차 React

---
+++ starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Post from './components/Post';
+
+function App() {
+  return (
+    <main>
+      <Post author="박소은" body="안녕하세요!!"/>
+      <Post author="소은" body="안녕히 가세요!!"/>
+    </main>
+  );
+}
+
+export default App;
